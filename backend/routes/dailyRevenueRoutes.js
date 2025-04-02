634684139131343cbc0286293dbb2b47453f5163
hamzaayarii <ayari.hamza1@esprit.tn>
2025-04-02 16:56:12 +0100

    minor fix

--- backend/routes/dailyRevenueRoutes.js
+++ backend/routes/dailyRevenueRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dailyRevenueController = require('../controllers/dailyRevenueController');
-const auth = require('../middleware/auth');
+const auth = require('../middlewares/auth');
 
 // Apply authentication middleware to all routes
 router.use(auth);
