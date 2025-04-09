690bad31f5839ee6887f1cd2c7c9c4352db59b15
Amine2026 <mohamedamine.lazzez@esprit.tn>
2025-04-09 19:12:16 +0100

    fix accountant being redirected to create business upon login

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,7 +5,7 @@ import Register from 'views/examples/Register.js';
 import Login from 'views/examples/Login.js';
 import Tables from 'views/examples/Tables.js';
 import Icons from 'views/examples/Icons.js';
-import AuthRoute, { AdminRoute, isUserAdmin } from 'components/AuthRoute.js';
+import AuthRoute, { AdminRoute, isUserAdmin, BusinessOwnerRoute } from 'components/AuthRoute.js';
 import PasswordReset from './views/examples/PasswordReset.js';
 import NewPassword from './views/examples/NewPassword.js';
 import Purchases from './views/examples/Purchases.js';
@@ -263,7 +263,7 @@ const routes = [
     path: '/business-registration',
     name: 'Start Your Business',
     icon: 'ni ni-briefcase-24 text-primary',
-    component: <BusinessRegistrationPage />,
+    component: <BusinessOwnerRoute><BusinessRegistrationPage /></BusinessOwnerRoute>,
     layout: '/standalone',
     showInSidebar: false,
   },
