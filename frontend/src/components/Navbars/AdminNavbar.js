0801b94c221ae04a73ddb13bcdd63b16966a593c
Amine2026 <mohamedamine.lazzez@esprit.tn>
2025-04-15 23:02:05 +0100

    Fix overlap and ensure profile pic/button are clickable.

--- frontend/src/components/Navbars/AdminNavbar.js
+++ frontend/src/components/Navbars/AdminNavbar.js
@@ -110,7 +110,7 @@ const AdminNavbar = (props) => {
 
   return (
     <>
-      <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+      <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main" style={{ zIndex: 2000, position: 'relative' }}>
         <Container fluid>
           <Link
             className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
